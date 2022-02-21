00e7d02495e178d3157035727fd8e97049ac4890
Alexis Bourget <alexis@harfanglab.fr>
2022-02-21 08:55:34 +0100

    shells: zsh: more comfortable default config

--- .config/shells/zsh/init.zsh
+++ .config/shells/zsh/init.zsh
@@ -1,17 +1,15 @@
 # History options
 
-setopt extended_history         # Record timestamp of command in HISTFILE
-setopt hist_find_no_dups        # Ignore duplicates when searching history
-setopt hist_ignore_all_dups     # Don't save duplicates
-setopt hist_ignore_space        # Ignore commands that start with space
-setopt hist_verify              # Show command with history expansion to user before running it
-setopt inc_append_history       # Write to HISTFILE as soon as the command are entered
-setopt share_history            # Share command history data
-
-# Globbing options
-
-setopt no_case_glob             # Ignore case when globbing
-setopt no_case_match            # Ignore case when matching
+setopt extended_history   # Record timestamp of command in HISTFILE
+setopt histfindnodups     # Ignore duplicates when searching history
+setopt histignorealldups  # Don't save duplicates
+setopt histignorespace    # Ignore commands that start with space
+setopt histverify         # Show command with history expansion to user before running it
+setopt incappend_history  # Write to HISTFILE as soon as the command are entered
+setopt nocaseglob         # Ignore case when globbing
+setopt nocasematch        # Ignore case when matching
+setopt numericglobsort    # Sort filenames numerically when it makes sense
+setopt sharehistory       # Share command history data
 
 bindkey ^A beginning-of-line
 bindkey ^E end-of-line
@@ -40,13 +38,25 @@ export HISTTIMEFORMAT="[%F %T] "
 
 if ( __shell_init_is_mac ); then
   [ -f /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh || true
+elif ( __shell_init_is_linux ); then
+  ## Plugins section: Enable fish style features
+  # Use syntax highlighting
+  source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  # Use history substring search
+  source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+  # bind UP and DOWN arrow keys to history substring search
+  zmodload zsh/terminfo
+  bindkey "$terminfo[kcuu1]" history-substring-search-up
+  bindkey "$terminfo[kcud1]" history-substring-search-down
+  bindkey '^[[A' history-substring-search-up
+  bindkey '^[[B' history-substring-search-down
 fi
 
 # Must be loaded before compinit
 # zmodload zsh/complist
 export fpath=( "$XDG_CONFIG_HOME/shells/zsh/completions" $fpath )
 
-autoload -Uz compinit
+autoload -Uz compinit colors
 mkdir -p "$XDG_CACHE_HOME/zsh"
 compinit -d "$XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION"
 
@@ -54,19 +64,26 @@ compinit -d "$XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION"
 source "$XDG_CONFIG_HOME/shells/zsh/completions/_pip3"
 source "$XDG_CONFIG_HOME/shells/zsh/completions/_pip"
 
-# setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
-# setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
-# setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
 
-# Allow you to select in a menu
-zstyle ':completion:*' menu select
+zstyle ':completion:*' menu select                          # Allow you to select in a menu
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'   # Case insensitive tab completion
+zstyle ':completion:*' rehash true                          # automatically find new executables in path
+# Speed up completions
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/comp-cache"
 
-# zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-# zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
-# zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
-# zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-# Colors for files and directory
-# zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
+colors
+
+# Color man pages
+export LESS_TERMCAP_mb=$'\E[01;32m'
+export LESS_TERMCAP_md=$'\E[01;32m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;47;34m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;36m'
+export LESS=-R
 
 alias shistory='history 1 | rg'
 
