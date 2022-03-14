7375841e4d83b2c8ee49064153fae9757df4a94b
Alexis Bourget <alexis.bourget@gmail.com>
2022-03-14 21:22:05 +0100

    shell: more complete helix update

--- .config/shells/common/utils.sh
+++ .config/shells/common/utils.sh
@@ -160,7 +160,9 @@ if [[ -z "${shell_utils_imported+x}" ]]; then
             && git checkout master \
             && git pull \
             && cargo install --force --path helix-term \
-            && ln -s "$PWD/runtime" "$helix_runtime" \
+            && ln -fs "$PWD/runtime" "$helix_runtime" \
+            && cd "$helix_runtime" \
+            && cd .. \
             && hx --grammar fetch \
             && hx --grammar build )
       fi
