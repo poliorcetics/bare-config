4ecacc6d8b9194a91edb976e725488269d6180db
Alexis Bourget <alexis@harfanglab.fr>
2022-03-16 11:41:35 +0100

    shell: functions: rework zs a little

--- .config/shells/common/functions.sh
+++ .config/shells/common/functions.sh
@@ -1,18 +1,30 @@
 # Aliases that have evolved into functions
 
 function hs() {
-    local target_path=$(sk --preview='bat -p --color always {1}');
-    if [[ -z "$target_path" ]]; then
-        return 1;
-    fi;
+    local target_path=$( sk \
+        --exit-0 \
+        --preview='bat -p --color always {1}' \
+        --preview-window='right:67%')
+    if [[ -z "$target_path" ]]; then return 1; fi;
+
     hx "$target_path"
 }
 
 
 function zs() {
-    local target_path=$(fd --type d --hidden --color never --strip-cwd-prefix $@ | sk --exit-0) # --preview='exa --long --all --group-directories-first --no-user {1}'
-    if [[ -z "$target_path" ]]; then
-        return 1;
-    fi;
+    local target_path=$(fd \
+        --type f \
+        --hidden \
+        --color never \
+        --strip-cwd-prefix $@ \
+        | sk \
+        --exit-0 \
+        --preview='bat -p --color always {1}' \
+        --preview-window='right:67%')
+    if [[ -z "$target_path" ]]; then return 1; fi;
+
+    target_path=$(dirname "$target_path")
+
+    if [[ $? != 0 ]] || [[ ! -d "$target_path" ]]; then return 1; fi
     z "$target_path"
 }
