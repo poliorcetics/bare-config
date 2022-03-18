f180435f4022903fe777fba7cdb0c3e1f7fbd6ac
Alexis Bourget <alexis@harfanglab.fr>
2022-03-18 15:35:48 +0100

    shell: add hr function to search before entering file

--- .config/shells/common/functions.sh
+++ .config/shells/common/functions.sh
@@ -1,10 +1,23 @@
 # Aliases that have evolved into functions
 
 function hs() {
-    local target_path=$( sk \
+    local target_path=$(sk \
         --exit-0 \
         --preview='bat -p --color always {1}' \
         --preview-window='right:67%')
+    if [[ -z "$target_path" ]]; then return 1; fi;
+
+    hx "$target_path"
+}
+
+
+function hr() {
+    local target_path=$(sk \
+        --exit-0 \
+        --ansi \
+        --interactive \
+        --cmd 'rg --color=always --vimgrep "{}"' \
+        | rg '^(.*:\d+:\d+):.*$' --replace '$1')
     if [[ -z "$target_path" ]]; then return 1; fi;
 
     hx "$target_path"
